8492180003a9b3f2ee1ddb2cf2378cfe1798c8b8
Van Phung <vanphung030792@gmail.com>
2024-02-16 07:00:05 -0500

    reverse projects, add live n gh link

---
+++ src/components/Project.tsx
@@ -0,0 +1,127 @@
+type ProjectProps = {
+    item: {
+        title: string;
+        description: string;
+        technologies: string[];
+        imgUrl: string;
+        url: string;
+        ghUrl: string;
+    };
+};
+
+const Project = ({ item }: ProjectProps) => {
+    return (
+        <div
+            className="
+                flex
+                flex-col
+                bg-primary
+                rounded-lg
+                shadow
+                border-4
+                border-primary
+                w-[85%]
+                h-[100%]
+                transition
+                duration-500
+                ease-in-out
+                transform
+                hover:scale-105
+                md:w-[95%]
+                lg:w-[85%]
+                xl:w-[90%]
+            "
+        >
+            {/* IMG */}
+            <img
+                src={
+                    item.imgUrl === ''
+                        ? '/portfolio/workInProgress.jpg'
+                        : item.imgUrl
+                }
+                alt={item.title}
+                className="
+                    rounded-t-lg
+                    border-b-2
+                    border-primary
+                "
+            />
+
+            {/* Details */}
+            <div
+                className="
+                    bg-light
+                    p-4
+                    flex
+                    flex-col
+                    gap-y-4
+                    rounded-b-lg
+                    grow
+                "
+            >
+                <h6
+                    className="
+                        uppercase
+                        text-dark
+                        font-medium
+                    "
+                >
+                    {item.title}
+                </h6>
+
+                {/* Github */}
+                <div
+                    className="
+                        flex
+                        flex-row
+                        gap-2
+                    "
+                >
+                    {item.url === '' ? (
+                        ''
+                    ) : (
+                        <a
+                            href={item.url}
+                            target="blank"
+                            className="btn-secondary link-btn"
+                        >
+                            Live
+                        </a>
+                    )}
+                    {item.ghUrl === '' ? (
+                        ''
+                    ) : (
+                        <a
+                            href={item.ghUrl}
+                            target="blank"
+                            className="btn-secondary link-btn"
+                        >
+                            Github
+                        </a>
+                    )}
+                </div>
+
+                {/* Description */}
+                <p className="text-sm font-light">{item.description}</p>
+
+                {/* Technologies */}
+                <div
+                    className="
+                        flex
+                        flex-row
+                        gap-2
+                        flex-wrap
+                    "
+                >
+                    {item.technologies.map((tech, id) => (
+                        <span key={id} className="button">
+                            {tech}
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,4 @@
-import Work from '../components/Work';
+import Project from '../components/Project';
 import projects from '../data/projects.json';
 
 const Portfolio = () => {
@@ -6,38 +6,38 @@ const Portfolio = () => {
         <div
             id="portfolio"
             className="
-        flex
-        flex-col
-        items-center
-        justify-center
-        gap-y-4
-        p-[10%]
-        bg-light
-        xl:pt-[5%]
-        xl:pb-[10%]
-      "
+                flex
+                flex-col
+                items-center
+                justify-center
+                gap-y-4
+                p-[10%]
+                bg-light
+                xl:pt-[5%]
+                xl:pb-[10%]
+            "
         >
             <h3>some works</h3>
             <h2>- MY PORTFOLIO - </h2>
             <div
                 className="
-          flex
-          flex-col
-          items-center
-          justify-items-center
-          gap-y-12
-          py-8
-          md:grid
-          md:grid-cols-2
-          md:gap-4
-          lg:gap-y-8
-          lg:gap-x-0
-          xl:grid-cols-3
-          xl:items-start
-        "
+                    flex
+                    flex-col-reverse
+                    items-center
+                    justify-items-center
+                    gap-y-12
+                    py-8
+                    md:grid
+                    md:grid-cols-2    
+                    md:gap-4
+                    lg:gap-y-8
+                    lg:gap-x-0
+                    xl:grid-cols-3
+                    xl:items-start
+                    "
             >
-                {projects.map((item, id) => (
-                    <Work id={id} item={item} />
+                {[...projects].reverse().map((item, id) => (
+                    <Project key={id} item={item} />
                 ))}
             </div>
         </div>
